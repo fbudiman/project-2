e5cc47f433033f1901f5ae672d8faf82b694bd57
Fran Budiman <fbudiman14@hotmail.com>
2015-12-28 01:13:54 -0800

    fixed some user issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :profile_pic_url, :email, :password, :password_confirmation)
   end
 
 
