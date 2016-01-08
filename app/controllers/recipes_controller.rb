7810062bdf20b3b8632cb691f8b21edcf21cf521
Fran Budiman <fbudiman14@hotmail.com>
2016-01-07 20:51:27 -0800

    styling recipe edit page and recipe pages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -47,6 +47,7 @@ class RecipesController < ApplicationController
   def edit
     @recipe = Recipe.find(params[:id])
     @restaurants = Restaurant.all
+    @user = User.new
 
     if !current_user
       flash[:error] = "You must be signed in to edit a recipe."
